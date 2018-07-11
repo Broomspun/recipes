e346944810a5b17d73ae328ecbb3f2f10c1428cb
Evan B <admin@storm-east.com>
2018-07-11 06:50:25 -0600

    01-7. 5. Adding the Shopping List Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeHoverDirective } from './shared/recipehover.directive';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { RecipeHoverDirective } from './shared/recipehover.directive';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Ingredient} from '../shared/ingredient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
+  getIngredients() {
+    return this.ingredients.slice(); // Copy
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
   constructor() { }
 }
