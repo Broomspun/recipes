f64fb2f43a56272ae6e7229e6eda823a5450e8f2
Evan B <admin@storm-east.com>
2018-07-13 11:29:54 -0600

    12 - Routing: 5. Fixing Page Reload Issues

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,16 +6,10 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSelect(feature: string) {
-    console.log(`Selected menu item "${feature}"`);
-    this.featureSelected.emit(feature);
   }
 
 }
