26307fee9c3e66ae8c98a06138b212f55b313f41
Evan B <admin@storm-east.com>
2018-07-13 11:57:31 -0600

    12 - Routing: 8. Configuring Route Parameters

--- src/app/recipies/recipe-detail/recipe-detail.component.ts
+++ src/app/recipies/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 import {Recipe} from '../../shared/recipe';
 import {RecipeService} from '../recipe.service';
 
@@ -8,11 +9,18 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((parms: Params) => {
+        this.id = +parms['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      });
   }
 
   onAddToShoppingList() {
