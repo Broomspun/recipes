b0d52ebc2ab2ec9565b0b97c1b2533b7a4606ab3
Evan B <admin@storm-east.com>
2018-07-14 15:03:06 -0600

    16 - Forms: 16. Submitting the Recipe Edit Form

--- src/app/recipies/recipe.service.ts
+++ src/app/recipies/recipe.service.ts
@@ -2,17 +2,19 @@ import {Injectable} from '@angular/core';
 import {Recipe} from '../shared/recipe';
 import {Ingredient} from '../shared/ingredient';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      'https://savorysweetlife.com/wp-content/uploads/brocolli-bacon-salad-recipe.jpg',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
@@ -20,13 +22,15 @@ export class RecipeService {
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      'https://www.chowstatic.com/assets/2015/01/31248_RecipeImage_Basic_yeast_donuts_2.jpg',
       [
         new Ingredient('Burns', 2),
         new Ingredient('Meat Fries', 1)
       ]
     )
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
@@ -40,5 +44,15 @@ export class RecipeService {
     this.slService.addIngredients(ingredients);
 
   }
-  constructor(private slService: ShoppingListService) { }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
 }
