ec05cac08284ce567390f9cc4a550c7dc8cccf5a
Evan B <admin@storm-east.com>
2018-07-14 12:37:45 -0600

    16 - Forms: 9. Allowing the Deletion of Shopping List Items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -55,6 +55,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.slForm.reset();
     this.editMode = false;
   }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
